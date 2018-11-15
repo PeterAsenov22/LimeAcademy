f9f28f0bcf060476df78d1ea3c62f1af38214c9f
PeterAsenov22 <peter12345@abv.bg>
2018-11-15 18:17:58 +0200

    CarToken contract updated

--- cars-etherlime/contracts/CarToken.sol
+++ cars-etherlime/contracts/CarToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.25;
 
-import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract CarToken is MintableToken, DetailedERC20 {
-  constructor(string _name, string _symbol, uint8 _decimals) public DetailedERC20(_name, _symbol, _decimals) {
+contract CarToken is ERC20Mintable, ERC20Detailed {
+  constructor(string _name, string _symbol, uint8 _decimals) public ERC20Detailed(_name, _symbol, _decimals) {
   }
 }
